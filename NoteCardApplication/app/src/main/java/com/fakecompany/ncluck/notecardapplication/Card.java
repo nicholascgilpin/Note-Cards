7e1e68245e7ac4aab3008074eb5f9af6c16e1d49
nano24432 <nano724@tamu.edu>
2015-09-05 19:44:05 -0500

    Created Card Class

---
+++ NoteCardApplication/app/src/main/java/com/fakecompany/ncluck/notecardapplication/Card.java
@@ -0,0 +1,16 @@
+package com.fakecompany.ncluck.notecardapplication;
+
+/**
+ * Created by ncluck on 9/5/15.
+ */
+
+public class Card {
+    String name;
+    String description;
+    // this is for the thumbnail variable
+
+    public void saveCard()
+    {
+
+    }
+}
--- NoteCardApplication/app/src/main/java/com/fakecompany/ncluck/notecardapplication/DisplayCards.java
+++ NoteCardApplication/app/src/main/java/com/fakecompany/ncluck/notecardapplication/DisplayCards.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 public class DisplayCards extends Activity {
 
@@ -11,6 +12,8 @@ public class DisplayCards extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_cards);
+        ListView carddisplay = (ListView)findViewById(R.id.expandableListView);
+
     }
 
     @Override
